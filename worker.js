c610f876ed4d8b23b096e345efc4bdc106e4f6ea
girkovarpa@protonmail.com <GirkovArpa>
2020-05-28 12:50:40 -0500

    offload to worker thread, don't update textarea until complete

---
+++ worker.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const keys = [];
+
+const generateKey = function () {
+  const chars = 'abcdefghijkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ23456789';
+  let key = 'VMK-';
+  for (let i = 0; i < 9; i++) {
+    if (i == 4) {
+      key += '-';
+      continue;
+    }
+    key += chars[~~(Math.random() * chars.length)];
+  }
+  return key;
+}
+
+const generateKeys = function () {
+  for (let i = 0; i < 100; i++) {
+    for (let j = 0; j < 100; j++) {
+      keys.push(generateKey());
+    }
+    console.log(`${i + 1}%`);
+  }
+}
+
+generateKeys();
+postMessage(keys);
